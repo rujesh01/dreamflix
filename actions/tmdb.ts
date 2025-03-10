a5a71eec17d62b5cc125c57b7f84b6a91d8a341b
mandip chaudhary <mandip.chaudhary2022@pcmgmt.edu.np>
2025-03-10 15:30:55 +0545

    top rated movies added

--- actions/tmdb.ts
+++ actions/tmdb.ts
@@ -1,26 +1,49 @@
-export const addToFavorites = async (movieId: number, favorite: boolean) => {
-    const TMDB_API_URL = "https://api.themoviedb.org/3/account/21870866/favorite";
+export const addToFavorites = async (media_id: number, favorite: boolean) => {
+    const TMDB_API_URL = `https://api.themoviedb.org/3/account/21870866/favorite`;
 
     try {
         const response = await fetch(TMDB_API_URL, {
             method: "POST",
             headers: {
-                Authorization: `Bearer ${process.env.TMDB_API_KEY}`, 
+                Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+                Accept: "application/json",
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
                 media_type: "movie",
-                media_id: movieId,
-                favorite: favorite, 
+                media_id,
+                favorite,
             }),
         });
 
         if (!response.ok) {
-            throw new Error("Failed to add to favorites");
+            throw new Error("Failed to update favorites");
         }
 
         return await response.json();
     } catch (error) {
-        throw new Error("Error adding movie to favorites");
+        throw new Error("Error updating favorites");
+    }
+};
+
+export const getTopRatedMovies = async (page: number = 1) => {
+    const TMDB_API_URL = `https://api.themoviedb.org/3/account/21870866/rated/movies?language=en-US&page=${page}&sort_by=created_at.asc`;
+
+    try {
+        const response = await fetch(TMDB_API_URL, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+                Accept: "application/json",
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch top-rated movies");
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw new Error("Error fetching top-rated movies");
     }
 };
---
+++ app/api/topRated/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { getTopRatedMovies } from "@/actions/tmdb";
+import { authMiddleware } from "@/actions/authMiddleware";
+
+export async function GET(req: Request) {
+    try {
+        authMiddleware(req);
+
+        const url = new URL(req.url);
+        const page = Number(url.searchParams.get("page")) || 1; // Get page number
+
+        const movies = await getTopRatedMovies(page);
+        return NextResponse.json(movies, { status: 200 });
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
