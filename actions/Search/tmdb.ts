606670ef8db0d18e89dcd6e250ac28d15f2df4b4
mandip chaudhary <mandip.chaudhary2022@pcmgmt.edu.np>
2025-03-11 14:06:39 +0545

    added

---
+++ actions/Search/tmdb.ts
@@ -0,0 +1,26 @@
+// Tv Search
+
+export const searchTVShows = async (query: string, page: number) => {
+    const response = await fetch(`https://api.themoviedb.org/3/search/tv?query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=${page}`, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${process.env.TMDB_API_KEY}`,
+            "Content-Type": "application/json",
+        },
+    });
+
+    return response.json();
+};
+
+// Seach Movies 
+export const searchMovies = async (query: string, page: number) => {
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=${page}`, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${process.env.TMDB_API_KEY}`,
+            "Content-Type": "application/json",
+        },
+    });
+
+    return response.json();
+};
